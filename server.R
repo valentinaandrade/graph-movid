674347cb3283427e621162fe074115b11cc6bd8b
Valentina Andrade <valentina.vasquez.a@ug.uchile.cl>
2020-09-22 15:31:17 -0300

    Update server.R

--- server.R
+++ server.R
@@ -16,7 +16,7 @@ server <- function(input, output) {
       hchart(.,
              "line",
              hcaes(semana_fecha, round(mediamovil,1), group = tipo)) %>%
-      hc_tooltip_n() %>%
+      hc_tooltip() %>%
       hc_xAxis(title = list(text = "")) %>%
       hc_yAxis(title = list(text = ""), labels = list(format = "{value}%"), min = 0) %>% 
       hc_subtitle(text = " Porcentaje de personas que cumplen con definición 
